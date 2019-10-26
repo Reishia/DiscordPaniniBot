e3d4233bf3a4ca85dd40e910913833d3bf8df82b
Reishia <alrerfr@gmail.com>
2019-10-26 23:11:28 +0200

    Ajout des joueurs de Cologne fini

--- players/playersBundesliga.js
+++ players/playersBundesliga.js
@@ -96,6 +96,168 @@ var playersBundesliga = [{
   nation: "France",
   club: "1.FC Cologne",
   image: "https://cdn.sofifa.org/players/10/20/182212.png"
+},
+{
+  id: "12",
+  name: "S.Terodde",
+  age: 31,
+  position: "BU",
+  nation: "Allemagne",
+  club: "1.FC Cologne",
+  image: "https://cdn.sofifa.org/players/10/20/183714.png"
+},
+{
+  id: "13",
+  name: "M.Höger",
+  age: 29,
+  position: "MDC",
+  nation: "Allemagne",
+  club: "1.FC Cologne",
+  image: "https://cdn.sofifa.org/players/10/20/198113.png"
+},
+{
+  id: "14",
+  name: "F.Kainz",
+  age: 26,
+  position: "MC",
+  nation: "Autriche",
+  club: "1.FC Cologne",
+  image: "https://cdn.sofifa.org/players/10/20/195038.png"
+},
+{
+  id: "15",
+  name: "L.Schaub",
+  age: 24,
+  position: "MOC",
+  nation: "Autriche",
+  club: "1.FC Cologne",
+  image: "https://cdn.sofifa.org/players/10/20/210489.png"
+},
+{
+  id: "16",
+  name: "J.Meré",
+  age: 22,
+  position: "DC",
+  nation: "Espagne",
+  club: "1.FC Cologne",
+  image: "https://cdn.sofifa.org/players/10/20/223033.png"
+},
+{
+  id: "17",
+  name: "B.Schmitz",
+  age: 24,
+  position: "DD",
+  nation: "Allemagne",
+  club: "1.FC Cologne",
+  image: "https://cdn.sofifa.org/players/10/20/222692.png"
+},
+{
+  id: "18",
+  name: "T.Kessler",
+  age: 33,
+  position: "GB",
+  nation: "Allemagne",
+  club: "1.FC Cologne",
+  image: "https://cdn.sofifa.org/players/10/20/158657.png"
+},
+{
+  id: "19",
+  name: "L.Sobiech",
+  age: 28,
+  position: "DC",
+  nation: "Allemagne",
+  club: "1.FC Cologne",
+  image: "https://cdn.sofifa.org/players/10/20/194053.png"
+},
+{
+  id: "20",
+  name: "V.Koziello",
+  age: 23,
+  position: "MC",
+  nation: "France",
+  club: "1.FC Cologne",
+  image: "https://cdn.sofifa.org/players/10/20/225199.png"
+},
+{
+  id: "21",
+  name: "M.Risse",
+  age: 29,
+  position: "MD",
+  nation: "Allemagne",
+  club: "1.FC Cologne",
+  image: "https://cdn.sofifa.org/players/10/20/189234.png"
+},
+{
+  id: "22",
+  name: "M.Bader",
+  age: 22,
+  position: "MD",
+  nation: "Allemagne",
+  club: "1.FC Cologne",
+  image: "https://cdn.sofifa.org/players/10/20/225692.png"
+},
+{
+  id: "23",
+  name: "C.Clemens",
+  age: 27,
+  position: "MG",
+  nation: "Allemagne",
+  club: "1.FC Cologne",
+  image: "https://cdn.sofifa.org/players/10/20/193098.png"
+},
+{
+  id: "24",
+  name: "N.Hauptmann",
+  age: 23,
+  position: "MOC",
+  nation: "Allemagne",
+  club: "1.FC Cologne",
+  image: "https://cdn.sofifa.org/players/10/20/234385.png"
+},
+{
+  id: "25",
+  name: "N.Katterbach",
+  age: 18,
+  position: "DG",
+  nation: "Allemagne",
+  club: "1.FC Cologne",
+  image: "https://cdn.sofifa.org/players/10/20/246666.png"
+},
+{
+  id: "26",
+  name: "D.Churlinov",
+  age: 18,
+  position: "AG",
+  nation: "Macédoine",
+  club: "1.FC Cologne",
+  image: "https://cdn.sofifa.org/players/10/20/251545.png"
+},
+{
+  id: "27",
+  name: "I.Jakobs",
+  age: 19,
+  position: "DG",
+  nation: "Allemagne",
+  club: "1.FC Cologne",
+  image: "https://cdn.sofifa.org/players/10/20/252293.png"
+},
+{
+  id: "28",
+  name: "B.Scott",
+  age: 20,
+  position: "GB",
+  nation: "États-Unis",
+  club: "1.FC Cologne",
+  image: "https://cdn.sofifa.org/players/10/20/241091.png"
+},
+{
+  id: "29",
+  name: "J.Krahl",
+  age: 19,
+  position: "GB",
+  nation: "Allemagne",
+  club: "1.FC Cologne",
+  image: "https://cdn.sofifa.org/players/10/20/244417.png"
 }];
 
 module.exports = playersBundesliga;
