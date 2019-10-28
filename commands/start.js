98c2831b8881fe687a9336c41c1db3862421a53d
Reishia <alrerfr@gmail.com>
2019-10-28 09:52:09 +0100

    ...

--- commands/start.js
+++ commands/start.js
@@ -3,19 +3,15 @@ const fs = require("fs");
 
 exports.run = (client, message) => { 
   client.money = require("../userData.json");
-  if (client.money[message.author.id].money === "") {
-    client.money[message.author.id] = {
-      money: 0,
-      lastDaily: "None"
-    };
+  client.money[message.author.id] = {
+    money: 0,
+    lastDaily: "None"
+  };
     
-    fs.writeFile("./userData.json", JSON.stringify(client.money, null, null), err => {
-      if (err) throw err;
-      message.channel.send(`Bienvenue sur FootCollection ${message.author}. Je te souhaite bonne chance !`);
-    });
-  } else {
-    message.channel.send("Vous avez déjà commencé votre aventure avec FootCollection !");
-  }
+  fs.writeFile("./userData.json", JSON.stringify(client.money, null, null), err => {
+    if (err) throw err;
+    message.channel.send(`Bienvenue sur FootCollection ${message.author}. Je te souhaite bonne chance !`);
+  });
   message.delete();
 };
 
