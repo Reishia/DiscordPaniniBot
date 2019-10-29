9823f1a3f0ff8303a54bb1ce70a5b39762998796
Reishia <alrerfr@gmail.com>
2019-10-29 16:16:53 +0100

    Wut

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,6 @@
-module.exports = () => {
+module.exports = client => {
   console.log("Je suis prêt !");
+  client.user.setActivity("FC Nantes - Paris FC", { type: "WATCHING" }
+    .then(presence => console.log(`Activité mise à ${presence.activity.name}`)))
+    .catch(console.error);
 };
